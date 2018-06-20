f77ded9eaf5b2befe012d56f3147b771cc8b052f
huangshaoguang <huangshaoguang>
2018-06-20 18:28:34 +0800

    添加高德地图资料及签名信息

--- app/src/main/java/com/littleant/carrepair/activies/MainActivity.java
+++ app/src/main/java/com/littleant/carrepair/activies/MainActivity.java
@@ -4,13 +4,55 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.amap.api.maps2d.MapView;
 import com.littleant.carrepair.R;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * 高德地图
+     */
+    private MapView mMapView = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mMapView != null) {
+            mMapView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mMapView != null) {
+            mMapView.onPause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mMapView != null) {
+            mMapView.onDestroy();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(mMapView != null) {
+            mMapView.onSaveInstanceState(outState);
+        }
     }
 }
