3f4aef5386e5e93be98830b6920aa560e6e018b1
sghuang <sghuang@github.com>
2018-08-09 21:14:17 +0800

    主页tab使用fragment实现

--- app/src/main/java/com/littleant/carrepair/activies/MainActivity.java
+++ app/src/main/java/com/littleant/carrepair/activies/MainActivity.java
@@ -1,32 +1,28 @@
 package com.littleant.carrepair.activies;
 
 import android.Manifest;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.amap.api.maps.AMap;
-import com.amap.api.maps.MapView;
-import com.amap.api.maps.model.MyLocationStyle;
 import com.littleant.carrepair.R;
+import com.littleant.carrepair.fragment.MainFragment;
 
 /**
  * 主页
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
     /**
      * 高德地图
      */
-    private MapView mMapView = null;
-    private TextView mRepair, mMaintain;
     private static final int permission_request_code = 10;
-    //初始化地图控制器对象
-    AMap aMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,60 +31,42 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, permission_request_code);
 
-        mMapView = findViewById(R.id.map);
-        mMapView.onCreate(savedInstanceState);
-
-        if (aMap == null) {
-            aMap = mMapView.getMap();
-        }
-
-        mRepair = findViewById(R.id.m_repair);
-        mRepair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "点击维修按钮", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        mMaintain = findViewById(R.id.m_maintain);
-        mMaintain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "点击保养按钮", Toast.LENGTH_SHORT).show();
-            }
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_fragment, new MainFragment(), MainFragment.class.getSimpleName());
+        transaction.commitAllowingStateLoss();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(mMapView != null) {
-            mMapView.onResume();
-        }
+//        if(mMapView != null) {
+//            mMapView.onResume();
+//        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(mMapView != null) {
-            mMapView.onPause();
-        }
+//        if(mMapView != null) {
+//            mMapView.onPause();
+//        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mMapView != null) {
-            mMapView.onDestroy();
-        }
+//        if(mMapView != null) {
+//            mMapView.onDestroy();
+//        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if(mMapView != null) {
-            mMapView.onSaveInstanceState(outState);
-        }
+//        if(mMapView != null) {
+//            mMapView.onSaveInstanceState(outState);
+//        }
     }
 
     @Override
@@ -97,12 +75,12 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == permission_request_code) {
             if(grantResults.length > 0 && permissions.length > 0) {
                 //获取定位权限
-                MyLocationStyle myLocationStyle;
-                myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-                myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-                aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-                //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-                aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+//                MyLocationStyle myLocationStyle;
+//                myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+//                myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+//                aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+//                //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+//                aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
             } else {
                 //获取定位权限失败
                 Toast.makeText(this, R.string.permission_deny_location, Toast.LENGTH_SHORT).show();
@@ -110,4 +88,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
