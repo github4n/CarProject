88d7ce677e5d7d3c49f94b361a5fd8a5449fb4af
sghuang <sghuang@github.comhuangshaoguang>
2018-09-05 18:59:34 +0800

    添加年检详情  订单详情

---
+++ app/src/main/java/com/littleant/carrepair/activies/AnnualCheckDetailActivity.java
@@ -0,0 +1,28 @@
+package com.littleant.carrepair.activies;
+
+import android.os.Bundle;
+
+import com.littleant.carrepair.R;
+
+public class AnnualCheckDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_annual_check_detail;
+    }
+
+    @Override
+    protected int getTitleId() {
+        return R.string.text_check_detail;
+    }
+}
